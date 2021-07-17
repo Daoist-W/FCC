64a7f8454337bd86ab01abdb98e3f81e0fa82ed6
Daoist-W <isikodon@googlemail.com>
2021-07-17 15:26:27 -0600

    initial upload

---
+++ phone-number-validator/phone-number-validator.js
@@ -0,0 +1,26 @@
+/* 
+This is the naming convention used in this program.
+____________________________________________________________________________________________________________________________________
+Type       | Example   | Description                                                                                                |
+Container  | myCont    | container to store non information temporarily in a function or scope limited loop. should not be global   |
+String     | sCipher   | 's' indicates string format                                                                                |
+Boolean    | bCheckIf  | 'b' indicates boolean format                                                                               |
+Integer    | numValue  | 'num' indicates a number, floating or integer                                                              |
+Array      | arrCont1  | 'arr' indicates an array                                                                                   |
+Object     | oPerson   | 'o' indicates an object                                                                                    |
+Function   | fnMyFunc  | 'fn' indicates function                                                                                    |
+____________________________________________________________________________________________________________________________________|
+*/
+
+// The function of this program is to return true if the input string looks like a valid US phone number.
+
+
+function telephoneCheck(str) {
+  return true;
+}
+
+telephoneCheck("555-555-5555");
+
+let charCode = NaN;
+
+console.log(charCode <= 77);
